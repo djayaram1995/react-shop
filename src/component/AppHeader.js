b578ec9bc8a1e2fbe37185daa394a78c1e61cf3c
unknown <jayaramdeenu@gmail.com>
2018-05-06 19:37:40 +0530

    Latest

--- src/component/AppHeader.js
+++ src/component/AppHeader.js
@@ -1,17 +1,67 @@
 import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 import logo from './comp.svg';
 import './AppHeader.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      name: "DJ"
+    };
+  }
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="container-fluid App-header">
-          <img src={logo} className="mr-2 App-logo" alt="logo" />
-          <h1 className="App-title">Computer Shop</h1>
-          
-        </header>
-      </div>
+       <Navbar className="App-header" light expand="md">
+          <NavbarBrand href="/">
+            <img src={logo} className="m-2 App-logo" alt="logo" />
+            <div className="offset">
+            <h1 className="App-title">Computer Shop</h1>
+            </div>
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">{ this.state.name }</NavLink>
+              </NavItem>
+             <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  More
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Sell With us
+                  </DropdownItem>
+                  <DropdownItem>
+                    Whole Sale
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <NavItem>
+                <NavLink href="/components/">Cart</NavLink>
+              </NavItem>
+              
+            </Nav>
+          </Collapse>
+        </Navbar>
     );
   }
 }
